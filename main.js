c0843b2689e03591879421cd78db278d82712a8b
Diego Veras <diego.veras96@hotmail.com>
2022-09-29 18:59:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,13 @@ function tocaSom(idAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-var i = 0;
 
-while (i < listaDeTeclas.length){
-    
-    const tecla = listaDeTeclas[i]
+for (var i = 0; i < listaDeTeclas.length; i++){
+    const tecla= listaDeTeclas[i];
     const som = tecla.classList[1];
     const id = `#som_${som}`;
-    
+
     tecla.onclick = function(){
         tocaSom(id);
     };
-    
-    i++
 }
